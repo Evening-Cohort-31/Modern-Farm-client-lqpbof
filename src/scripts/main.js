5fa90a5b9f1d27ddd128a63359e3d85b31899eca
Nick Segui <nicksegui5@gmail.com>
2025-07-31 18:44:17 -0500

    Seed modules added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,6 @@
-console.log("Welcome to the main module")
-
+import { createPlan } from "./plan.js";
+import { createAsparagus } from "./seeds/asparagus.js";
+const yearlyPlan = createPlan();
+const asparagusSeed = createAsparagus();
+console.log("Welcome to the main module");
+console.log(asparagusSeed);
