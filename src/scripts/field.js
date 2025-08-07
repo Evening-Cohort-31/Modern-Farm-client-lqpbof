14bc69a3cbac67509cd110f6e43ca263cf2b7159
Lea Edwards <leaxedw@gmail.com>
2025-08-07 18:42:40 -0500

    added some code to the field.js module

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,26 +1,34 @@
 // This module will import and store the seed object from "tractor.js"
 
-import { createAsparagus } from "./seeds/asparagus.js"
+import { createAsparagus } from "./seeds/asparagus.js";
 
 // Create an empty array named: "fieldArray" (this array should not be exported)
 
-const fieldArray = []
+const fieldArray = [];
 
 /* Create and export a function called "addPlant" that accepts a seed objects as input and stores 
 the seed inside 'fieldArray' */
 
 export const addPlant = (seedObject) => {
-    // Corn with return an array of objects- which will need to be iterated so the objects can be add individually.
-
     
+  // Corn will return an array of objects- which will need to be iterated so the objects can be add individually.
 
-    fieldArray.push(seedObject)
-}
+  // Include an if statement or function to detect whether the argument is an array and iterate/map then push.
 
-// Create and export a function called "usePlants" that returns a copy of fieldArray. 
+  // If the argument is not a array then it will just be pushed into the fieldArray
 
-export const usePlants = () => fieldArray.map()
+  if (seedObject.isArray === true) {
+    for (const seed of seedObject) {
+      fieldArray.push(seed);
+    }
+  } else {
+    fieldArray.push(seedObject);
+  }
+  return fieldArray // unsure if this is the right thing to return atm
+};
 
+// Create and export a function called "usePlants" that returns a copy of fieldArray.
 
-// This module will export data from the plants array to the 'harvester.js' module. 
+export const usePlants = () => fieldArray.map();
 
+// This module will export data from the plants array to the 'harvester.js' module.
