ae34e37c37ca7003fda0e31833973fae9d103574
Kenny Panyavong <elp.kenny@gmail.com>
2025-08-07 20:34:51 -0500

    Defined and export renderCatalog to render HTML to DOM, import functions to main for testing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,15 @@
+import { renderCatalog } from "./catalog.js";
+import { usePlants } from "./field.js";
+import { harvestPlants } from "./harvester.js";
 import { createPlan } from "./plan.js";
 import { createAsparagus } from "./seeds/asparagus.js";
+
+
 const yearlyPlan = createPlan();
 const asparagusSeed = createAsparagus();
 console.log("Welcome to the main module");
 console.log(asparagusSeed);
+
+const plantsInField = usePlants()
+const harvestedFood = harvestPlants(plantsInField)
+renderCatalog(harvestedFood)
