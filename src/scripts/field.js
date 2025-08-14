55c072fff5f654a36e95db4da0bfde5ab5e2e481
Lea Edwards <leaxedw@gmail.com>
2025-08-14 18:12:44 -0500

    Adding changes so I can merge

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,6 +1,7 @@
 // This module will import and store the seed object from "tractor.js"
 
 import { createAsparagus } from "./seeds/asparagus.js";
+import { createSoybean } from "./seeds/soybean.js";
 
 // Create an empty array named: "fieldArray" (this array should not be exported)
 
@@ -10,6 +11,7 @@ const fieldArray = [];
 the seed inside 'fieldArray' */
 
 export const addPlant = (seedObject) => {
+  
   // Corn will return an array of objects- which will need to be iterated so the objects can be add individually.
 
   // Include an if statement or function to detect whether the argument is an array and iterate/map then push.
@@ -29,7 +31,9 @@ export const addPlant = (seedObject) => {
 // Create and export a function called "usePlants" that returns a copy of fieldArray.
 
 export const usePlants = () => {
-  const exportField = [...fieldArray];
+  const exportField = structuredClone(fieldArray)
   return exportField;
 };
 // This module will export data from the plants array to the 'harvester.js' module.
+
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,5 @@ import { createPlan } from "./plan.js";
 import { createAsparagus } from "./seeds/asparagus.js";
 const yearlyPlan = createPlan();
 const asparagusSeed = createAsparagus();
-console.log("Welcome to the main module");
+// console.log("Welcome to the main module");
 console.log(asparagusSeed);
