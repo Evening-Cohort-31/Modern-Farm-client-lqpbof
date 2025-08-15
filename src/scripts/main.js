737cfc258df293bae3058d85c83cc820f0a77e6b
Kenny Panyavong <elp.kenny@gmail.com>
2025-08-14 19:13:47 -0500

    Invoke plantSeeds with yearlyPlan

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,14 @@ import { usePlants } from "./field.js";
 import { harvestPlants } from "./harvester.js";
 import { createPlan } from "./plan.js";
 import { createAsparagus } from "./seeds/asparagus.js";
+import { plantSeeds } from "./tractor.js";
 
 
 const yearlyPlan = createPlan();
 const asparagusSeed = createAsparagus();
 // console.log("Welcome to the main module");
-console.log(asparagusSeed);
-
+// console.log(asparagusSeed);
+plantSeeds(yearlyPlan)
 const plantsInField = usePlants()
 const harvestedFood = harvestPlants(plantsInField)
 renderCatalog(harvestedFood)
